554e31a6f9cd10932fc6e3dec992c1d7fb7fcbd8
nuxarch <misbaqul4sfi4k>
2023-06-21 11:29:31 +0700

    testing driver ok

--- src/main.cpp
+++ src/main.cpp
@@ -1,91 +1,42 @@
+// BLDC driver standalone example
 #include <SimpleFOC.h>
-#include "DRV8301.h"
-#include "polyDrive.h"
 
-MagneticSensorI2C sensor = MagneticSensorI2C(AS5600_I2C);
 
-xTaskHandle taskThrottleHandle;
-xTaskHandle taskTestSetPointHandle;
-xTaskHandle taskProtectionHandle;
+// BLDC driver instance
+BLDCDriver3PWM driver = BLDCDriver3PWM(25, 26, 27,13);
 
-polyDrive myboard;
-void onMotor(char *cmd) { myboard.command.motor(&myboard.motor, cmd); }
-void doTarget(char *cmd) { myboard.command.scalar(&myboard.target_velocity, cmd); }
+void setup() {
+  
+  pinMode(4, INPUT_PULLUP);
+  pinMode(14, OUTPUT);
+  pinMode(15, OUTPUT);
+  digitalWrite(14, HIGH);
+  digitalWrite(15, HIGH);
+  // pwm frequency to be used [Hz]
+  // for atmega328 fixed to 32kHz
+  // esp32/stm32/teensy configurable
+  driver.pwm_frequency = 50000;
+  // power supply voltage [V]
+  driver.voltage_power_supply = 12;
+  // Max DC voltage allowed - default voltage_power_supply
+  driver.voltage_limit = 12;
 
-void taskTestSetPoint(void *parameter)
-{
-    myboard.taskSet();
-}
-void taskReadTemp(void *parameter)
-{
-    myboard.taskProtection();
-}
-void readTemp(char *cmd)
-{
-    // myboard.taskProtection();
-    xTaskCreate(taskReadTemp, "task read temp", 4000, NULL, 1, &taskProtectionHandle);
-}
+  // driver init
+  driver.init();
 
-void setControl(char *cmd)
-{
-    xTaskCreate(taskTestSetPoint, "task manual", 4000, NULL, 1, &taskTestSetPointHandle);
-}
-void setControlOff(char *cmd)
-{
-    vTaskDelete(taskTestSetPointHandle);
+  // enable driver
+  driver.enable();
+
+  _delay(1000);
+  
+  Serial.begin(115200);;
+  Serial.println("test");
 }
 
-void setup()
-{
-    Serial.begin(115200);
-    
-    myboard.initBoard(36);
-    myboard.initDRV8313();
-    // sensor.quadrature = Quadrature::OFF;
-    // sensor.pullup = Pullup::USE_EXTERN;
-    // sensor.enableInterrupts(doA, doB);
-
-    // configure i2C
-    Wire.setClock(400000);
-    // initialise magnetic sensor hardware
-    sensor.init();
-
-    myboard.limits(1, 50, 1, 2);
-    myboard.motor.linkSensor(&sensor);
-    // for A2212
-    myboard.vel_PID(0.1, 0.01, 0.001, 200, 5, 0.3);
-
-    myboard.angle_PID(16,0,0,5,25,0.01);
-    myboard.motor.useMonitoring(Serial);
-
-    // myboard.motor.controller = MotionControlType::velocity_openloop;
-    // myboard.motor.controller = MotionControlType::angle_openloop;
-    // myboard.motor.controller = MotionControlType::torque;
-    myboard.motor.controller = MotionControlType::velocity;
-    // myboard.motor.controller = MotionControlType::angle;
-    myboard.motor.init();
-    int i = 0;
-    while (myboard.motor.initFOC() != 1){
-        delay(1000);
-        Serial.println("trying "+String(++i));
-        digitalWrite(RESET_PIN, LOW);delay(500);
-        digitalWrite(RESET_PIN, HIGH);
-        myboard.motor.init();
-        if (i >=3)
-        {
-            Serial.println("please restart");
-            while(1);
-        }
-        
-    }
-    myboard.command.add('T', doTarget, "target velocity");
-    myboard.command.add('M', onMotor, "on motor command");
-    myboard.command.add('S', setControl, "manual test point");
-    myboard.command.add('s', setControlOff, "manual test point off");
-    myboard.command.add('r', readTemp, "monitor temperature safety");
-}
-
-void loop()
-{
-    myboard.run();
+void loop() {
+    // setting pwm
+    // phase A: 3V
+    // phase B: 6V
+    // phase C: 5V
+    driver.setPwm(3,6,12);
 }
